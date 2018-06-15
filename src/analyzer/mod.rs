3fb31d5471a7ae614bbd0672f947ac09d1b5d026
Georgy Angelov <georgyangelov@gmail.com>
2018-06-15 21:25:44 +0300

    Support assignment constraints in the type analyzer

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -2,13 +2,17 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
+use std::iter::FromIterator;
+
+use std::rc::Rc;
+use std::borrow::Borrow;
+
 use super::ir::Type;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Constraint {
     Assert { var: Var, types: PossibleTypes },
-    // VarAssign { from: Var, to: Var },
-    // ValAssign { var: Var, t: Type }
+    Assignment { from: Var, to: Var }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -17,7 +21,7 @@ pub struct Var(i32);
 pub struct Analyzer {
     // TODO: Is this vec needed? We can use `next_var_id` and construct Vars ourselves
     vars: Vec<Var>,
-    constraints: Vec<Constraint>,
+    constraints: Vec<Rc<Constraint>>,
 
     next_var_id: i32
 }
@@ -33,18 +37,15 @@ pub struct Solution {
     pub types: HashMap<Var, Type>
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum PossibleTypes {
     All,
-    Some(HashSet<Type>)
+    Some(Vec<Type>)
 }
 
 impl PossibleTypes {
     fn only(t: Type) -> Self {
-        let mut set = HashSet::new();
-        set.insert(t);
-
-        PossibleTypes::Some(set)
+        PossibleTypes::Some(vec![t])
     }
 
     fn intersect(&self, other: &PossibleTypes) -> PossibleTypes {
@@ -53,9 +54,34 @@ impl PossibleTypes {
             &PossibleTypes::Some(ref self_types) => {
                 match other {
                     &PossibleTypes::All => self.clone(),
-                    &PossibleTypes::Some(ref other_types) => PossibleTypes::Some(
-                        self_types.intersection(other_types).cloned().collect()
-                    )
+                    &PossibleTypes::Some(ref other_types) => {
+                        let self_type_set: HashSet<Type> = HashSet::from_iter(self_types.iter().cloned());
+                        let other_type_set: HashSet<Type> = HashSet::from_iter(other_types.iter().cloned());
+
+                        PossibleTypes::Some(self_type_set.intersection(&other_type_set).cloned().collect())
+                    }
+                }
+            }
+        }
+    }
+
+    fn equals(&self, other: &PossibleTypes) -> bool {
+        match self {
+            &PossibleTypes::All => {
+                match other {
+                    &PossibleTypes::All => true,
+                    _ => false
+                }
+            },
+            &PossibleTypes::Some(ref self_types) => {
+                match other {
+                    &PossibleTypes::All => false,
+                    &PossibleTypes::Some(ref other_types) => {
+                        let self_type_set: HashSet<Type> = HashSet::from_iter(self_types.iter().cloned());
+                        let other_type_set: HashSet<Type> = HashSet::from_iter(other_types.iter().cloned());
+
+                        self_type_set == other_type_set
+                    }
                 }
             }
         }
@@ -65,8 +91,96 @@ impl PossibleTypes {
 #[derive(Debug)]
 struct SolutionDraft {
     possible_types: HashMap<Var, PossibleTypes>,
-    var_constraints: HashMap<Var, Vec<Constraint>>,
-    next: VecDeque<Var>
+
+    var_constraints: HashMap<Var, Vec<Rc<Constraint>>>,
+
+    next: VecDeque<Rc<Constraint>>,
+    to_be_revalidated: HashSet<Rc<Constraint>>
+}
+
+impl SolutionDraft {
+    pub fn new(vars: &Vec<Var>, constraints: &Vec<Rc<Constraint>>) -> Self {
+        let mut result = Self {
+            possible_types: Self::build_initial_possible_types(vars),
+
+            var_constraints: HashMap::new(),
+
+            next: VecDeque::new(),
+            to_be_revalidated: HashSet::new()
+        };
+
+        result.build_constraints(vars, constraints);
+        result.build_initial_next(constraints);
+
+        result
+    }
+
+    pub fn possible_types_for(&self, var: Var) -> &PossibleTypes {
+        self.possible_types.get(&var)
+            .expect("possible_types must include all variables")
+    }
+
+    pub fn revalidate_var(&mut self, var: Var) {
+        let constraints = self.var_constraints.get(&var)
+            .expect("var_constraints must include all vars")
+            .clone();
+
+        for constraint in &constraints {
+            self.revalidate_constraint(constraint);
+        }
+    }
+
+    pub fn revalidate_constraint(&mut self, constraint: &Rc<Constraint>) {
+        if self.to_be_revalidated.contains(constraint) {
+            return;
+        }
+
+        self.next.push_back(constraint.clone());
+        self.to_be_revalidated.insert(constraint.clone());
+    }
+
+    fn build_initial_possible_types(vars: &Vec<Var>) -> HashMap<Var, PossibleTypes> {
+        let mut result = HashMap::new();
+
+        for var in vars.iter().cloned() {
+            result.insert(var, PossibleTypes::All);
+        }
+
+        result
+    }
+
+    fn build_constraints(&mut self, vars: &Vec<Var>, constraints: &Vec<Rc<Constraint>>) {
+        for var in vars.iter().cloned() {
+            self.var_constraints.insert(var, Vec::new());
+        }
+
+        for constraint in constraints {
+            match constraint.borrow() {
+                &Constraint::Assert { var, .. } => self.var_constraints.get_mut(&var)
+                    .expect("var_constraints must contain all vars")
+                    .push(constraint.clone()),
+
+                &Constraint::Assignment { to, from } => {
+                    self.var_constraints.get_mut(&to)
+                        .expect("var_constraints must contain all vars")
+                        .push(constraint.clone());
+
+                    self.var_constraints.get_mut(&from)
+                        .expect("var_constraints must contain all vars")
+                        .push(constraint.clone());
+                }
+            };
+        }
+    }
+
+    fn build_initial_next(&mut self, constraints: &Vec<Rc<Constraint>>) {
+        for constraint in constraints {
+            match constraint.borrow() {
+                &Constraint::Assert { .. } => self.revalidate_constraint(constraint),
+                _ => ()
+            };
+        }
+    }
 }
 
 impl Analyzer {
@@ -88,86 +202,82 @@ impl Analyzer {
     }
 
     pub fn assert(&mut self, var: Var, t: Type) {
-        self.constraints.push(Constraint::Assert {
+        let constraint = Rc::new(Constraint::Assert {
             var,
             types: PossibleTypes::only(t)
         });
+
+        self.constraints.push(constraint);
     }
 
     pub fn assert_multi(&mut self, var: Var, types: Vec<Type>) {
-        self.constraints.push(Constraint::Assert {
+        let constraint = Rc::new(Constraint::Assert {
             var,
             types: PossibleTypes::Some(types.iter().cloned().collect())
-        })
+        });
+
+        self.constraints.push(constraint);
+    }
+
+    pub fn assign(&mut self, to: Var, from: Var) {
+        let constraint = Rc::new(Constraint::Assignment {
+            from,
+            to
+        });
+
+        self.constraints.push(constraint);
     }
 
     pub fn solve(&self) -> Result<Solution, AnalyzerError> {
-        let mut draft = SolutionDraft {
-            possible_types: self.build_initial_possible_types(),
-            var_constraints: self.build_var_constraints(),
-            next: VecDeque::new()
-        };
+        let mut draft = SolutionDraft::new(&self.vars, &self.constraints);
 
-        for constraint in &self.constraints {
-            match constraint {
-                &Constraint::Assert { var, .. } => draft.next.push_back(var)
-            };
-        }
+        while let Some(constraint) = draft.next.pop_front() {
+            Self::apply_constraint(&mut draft, &constraint);
 
-        while let Some(var) = draft.next.pop_front() {
-            let constraints = draft.var_constraints.get(&var)
-                .expect("var_constraints must contain all vars")
-                // TODO: Figure out a way to not clone this
-                .clone();
-
-            for constraint in constraints {
-                Self::apply_constraint(&mut draft, &constraint);
-            }
+            draft.to_be_revalidated.remove(&constraint);
         }
 
         self.present_solution(&mut draft)
     }
 
-    fn build_initial_possible_types(&self) -> HashMap<Var, PossibleTypes> {
-        let mut result = HashMap::new();
-
-        for var in self.vars.iter().cloned() {
-            result.insert(var, PossibleTypes::All);
-        }
-
-        result
-    }
-
-    fn build_var_constraints(&self) -> HashMap<Var, Vec<Constraint>> {
-        let mut result = HashMap::new();
-
-        for var in self.vars.iter().cloned() {
-            result.insert(var, Vec::new());
-        }
-
-        for constraint in &self.constraints {
-            match constraint {
-                &Constraint::Assert { var, .. } => result.get_mut(&var)
-                    .expect("var_constraints must contain all vars")
-                    .push(constraint.clone())
-            }
-        }
-
-        result
-    }
-
     // TODO: Ability to fail early
-    fn apply_constraint(draft: &mut SolutionDraft, constraint: &Constraint) {
-        match constraint {
+    fn apply_constraint(draft: &mut SolutionDraft, constraint: &Rc<Constraint>) {
+        match constraint.borrow() {
             &Constraint::Assert { var, ref types } => {
-                let next_possible = {
-                    let currently_possible = draft.possible_types.get(&var)
-                        .expect("possible_types must contain all vars");
+                let mut types_changed;
+                let mut next_possible;
 
-                    currently_possible.intersect(types)
-                };
+                {
+                    let currently_possible = draft.possible_types_for(var);
 
-                draft.possible_types.insert(var, next_possible);
+                    next_possible = currently_possible.intersect(types);
+                    types_changed = !currently_possible.equals(&next_possible);
+                }
+
+                if types_changed {
+                    // TODO: Make this a method?
+                    draft.possible_types.insert(var, next_possible);
+                    draft.revalidate_var(var);
+                }
+            },
+
+            &Constraint::Assignment { to, from } => {
+                // TODO: Fail fast and with appropriate error
+                let current_from_types = draft.possible_types_for(from).clone();
+                let current_to_types = draft.possible_types_for(to).clone();
+
+                let next_to_types = current_to_types.intersect(&current_from_types);
+                let next_from_types = next_to_types.clone();
+
+                if !current_to_types.equals(&next_to_types) {
+                    draft.possible_types.insert(to, next_to_types);
+                    draft.revalidate_var(to);
+                }
+
+                if !current_from_types.equals(&next_from_types) {
+                    draft.possible_types.insert(from, next_from_types);
+                    draft.revalidate_var(from);
+                }
             }
         }
     }
@@ -178,8 +288,7 @@ impl Analyzer {
         };
 
         for var in self.vars.iter().cloned() {
-            let possible_types = draft.possible_types.get(&var)
-                .expect("possible_types must contain all vars");
+            let possible_types = draft.possible_types_for(var);
 
             match possible_types {
                 &PossibleTypes::All => return Err(AnalyzerError::VarHasNoType(var)),
@@ -188,7 +297,7 @@ impl Analyzer {
                     if types.len() == 0 {
                         return Err(AnalyzerError::VarHasNoType(var));
                     } else if types.len() > 1 {
-                        return Err(AnalyzerError::VarMayHaveManyTypes(var, types.clone()));
+                        return Err(AnalyzerError::VarMayHaveManyTypes(var, types.iter().cloned().collect()));
                     } else {
                         solution.types.insert(var, types.iter().cloned().next().unwrap());
                     }
--- tests/analyzer/mod.rs
+++ tests/analyzer/mod.rs
@@ -4,6 +4,8 @@ use photon::ir::Type;
 
 #[test]
 fn test_vars_must_have_type() {
+    // Tests `var` => Error
+
     let mut analyzer = Analyzer::new();
     let var = analyzer.new_var();
 
@@ -15,6 +17,8 @@ fn test_vars_must_have_type() {
 
 #[test]
 fn test_simple_assert() {
+    // Tests `var:Int`
+
     let mut analyzer = Analyzer::new();
     let var = analyzer.new_var();
 
@@ -27,6 +31,8 @@ fn test_simple_assert() {
 
 #[test]
 fn test_conflicting_asserts() {
+    // Tests `var:Int`, `var:Bool` => Error
+
     let mut analyzer = Analyzer::new();
     let var = analyzer.new_var();
 
@@ -41,6 +47,8 @@ fn test_conflicting_asserts() {
 
 #[test]
 fn test_assert_intersection_inference() {
+    // Tests `var:Int,Bool`, `var:Float,Bool` => `var:Bool`
+
     let mut analyzer = Analyzer::new();
     let var = analyzer.new_var();
 
@@ -49,10 +57,82 @@ fn test_assert_intersection_inference() {
 
     let result = analyzer.solve();
 
+    assert_result_type(result, var, Type::Bool);
+}
+
+#[test]
+fn test_assignment_inference() {
+    // Tests `to = from:Int` => `to:Int`
+
+    let mut analyzer = Analyzer::new();
+    let from = analyzer.new_var();
+    let to = analyzer.new_var();
+
+    analyzer.assert(from, Type::Int);
+    analyzer.assign(to, from);
+
+    let result = analyzer.solve();
+
+    assert_result_type(result, to, Type::Int);
+}
+
+#[test]
+fn test_transitive_assignment_inference() {
+    // Tests `a = b = c:Int` => `a:Int`, `b:Int`
+
+    let mut analyzer = Analyzer::new();
+    let a = analyzer.new_var();
+    let b = analyzer.new_var();
+    let c = analyzer.new_var();
+
+    analyzer.assert(c, Type::Int);
+    analyzer.assign(a, b);
+    analyzer.assign(b, c);
+
+    let result = analyzer.solve();
+
+    assert_result_type(result, a, Type::Int);
+}
+
+#[test]
+fn test_constraint_loop() {
+    // Tests `a = b:Int`, `b = a` => `a:Int` and no infinite looping :)
+
+    let mut analyzer = Analyzer::new();
+    let a = analyzer.new_var();
+    let b = analyzer.new_var();
+
+    analyzer.assert(b, Type::Int);
+    analyzer.assign(a, b);
+    analyzer.assign(b, a);
+
+    let result = analyzer.solve();
+
+    assert_result_type(result, a, Type::Int);
+}
+
+#[test]
+fn test_assignment_back_propagation() {
+    // Tests `to:Int,Bool = from:Int`
+
+    let mut analyzer = Analyzer::new();
+    let from = analyzer.new_var();
+    let to = analyzer.new_var();
+
+    analyzer.assert_multi(from, vec![Type::Int, Type::Bool]);
+    analyzer.assert(to, Type::Int);
+    analyzer.assign(to, from);
+
+    let result = analyzer.solve();
+
+    assert_result_type(result, from, Type::Int);
+}
+
+fn assert_result_type(result: Result<Solution, AnalyzerError>, var: Var, t: Type) {
     assert!(result.is_ok());
 
     let types = result.unwrap().types;
     let var_type = *types.get(&var).unwrap();
 
-    assert_eq!(Type::Bool, var_type);
+    assert_eq!(t, var_type);
 }
