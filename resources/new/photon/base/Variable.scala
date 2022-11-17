a399bd0009ab50133e1b9e56abf97c36e598830f
Georgy Angelov <georgyangelov@gmail.com>
2022-11-18 00:12:14 +0200

    Remove some unused code from previous rewrites

--- resources/new/photon/base/Variable.scala
+++
@@ -1,6 +0,0 @@
-package photon.base
-
-class VarName(val originalName: String)
-class EVarName(originalName: String) extends VarName(originalName)
-
-case class Variable(name: EVarName, value: EValue)
--- resources/new/photon/lib/ScalaExtensions.scala
+++
@@ -1,7 +0,0 @@
-package photon.lib
-
-object ScalaExtensions {
-  implicit class IterableSetExtensions[T](seqOfSets: Iterable[Set[T]]) {
-    def unionSets: Set[T] = seqOfSets.fold(Set.empty) { case (a, b) => a ++ b }
-  }
-}
